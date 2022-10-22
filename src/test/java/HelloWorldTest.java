8502c628138db10438f2714cb3088700831d554b
Marat <marat1989.ne@gmail.com>
2022-10-22 20:11:09 +0300

    Ex_5

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,9 +1,25 @@
+import io.restassured.RestAssured;
+import io.restassured.http.Headers;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HelloWorldTest {
 
     @Test
-    public void testHelloWorld(){
-        System.out.println("Hello World");
+    public void RestAssured(){
+
+
+        JsonPath response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_json_homework")
+                .jsonPath();
+
+
+        ArrayList answer = response.get("messages");
+        System.out.println(answer.get(1));
     }
 }
