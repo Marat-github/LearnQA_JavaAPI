c2313521525d791e3075acd94ed21308106aa41c
Marat <marat1989.ne@gmail.com>
2022-10-22 23:53:29 +0300

    Ex_8

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -4,6 +4,7 @@ import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -14,6 +15,38 @@ public class HelloWorldTest {
     public void RestAssured() throws InterruptedException {
 
         JsonPath response = RestAssured
+                .get("https://playground.learnqa.ru/ajax/api/longtime_job")
+                .jsonPath();
+
+        String token = response.get("token");
+
+        response.prettyPrint();
+        //System.out.println(token);
+
+        String status = null;
+
+        boolean flag = true;
+
+        while(flag){
+            response = RestAssured
+                    .given()
+                    .queryParam("token", token)
+                    .get("https://playground.learnqa.ru/ajax/api/longtime_job")
+                    .jsonPath();
+
+            status = response.get("status");
+            
+            if(status.equals("Job is ready")){
+                System.out.println(status);
+                flag = false;
+            } else {
+                Thread.sleep(3000);
+                System.out.println("Waiting.....");
+            }
+        }
+
+
+        /*JsonPath response = RestAssured
                 .get("https://playground.learnqa.ru/ajax/api/longtime_job")
                 .jsonPath();
 
@@ -46,6 +79,6 @@ public class HelloWorldTest {
         //job = status.equals("Job is NOT ready");
 
         response.prettyPrint();
-        //System.out.println(job);
+        //System.out.println(job);*/
     }
 }
